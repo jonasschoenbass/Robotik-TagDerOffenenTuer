da713f7fa13d75777c0d7d73321efdec948483b3
Alexander Resch <alexander@resch-web.at>
2023-01-25 14:31:45 +0100

    project 1

--- programms/Projekt 1/translations/translation_en_US.ts
+++ programms/Projekt 1/translations/translation_en_US.ts
@@ -1,3 +1,18 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_US"/>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Face Reco./Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hi, Ithink I don't know you yet! Please tell me your name...</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hi, Ithink I don't know you yet! Please tell me your name...</translation>
+        </message>
+    </context>
+</TS>
